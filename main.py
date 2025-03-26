ff1ab7fe12acce536022961652ccc50b2fba97f1
Paulo Vieira <paulo1vieira.2004@gmail.com>
2025-03-26 10:54:43 +0100

    stuff

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from simpn.simulator import SimProblem, SimToken
-from numpy import random
+from random import expovariate as exp, uniform as unif
 from simpn.reporters import SimpleReporter
 from simpn.visualisation import Visualisation
-
+import simpn.prototypes as prototype
 
 # Variable Declaration 
 
@@ -12,32 +12,73 @@ mu = 1/10 # Service rate of an instructor
 instruction = SimProblem()
 
 student_groups = instruction.add_var('student_group')
-instructors = instruction.add_var('instructor')
 
 """
 Initial State -> One instructor and one student group waiting to be assigned
 """
 
-student_groups.put("c1")
-instructors.put("i")
+# Simulation -> Student Group Leave Queue 
 
-# Simulation 
+left = instruction.add_var('left')
+waiting = instruction.add_var('waiting')
+instructor = instruction.add_var('instructor')
+time_var = instruction.add_var("time")
 
-def arrive(a):
-    return [SimToken(f'c{str(a)}'), SimToken(a + 1, delay=random.exponential(scale=mu))]
+instructor.put("i")
 
-instruction.add_event([student_groups], [student_groups], arrive)
-
-def start(c1, i):
-    return [SimToken((c1, i), delay=random.uniform(15, 30))]
-
-instruction.add_event([student_groups, instructors], [student_groups, instructors], start)
+prototype.BPMNStartEvent(instruction, [], [waiting], "arrive", lambda: exp(lam))
 
 
-def end_service(c1, i):
-    return [SimToken(c1), SimToken(i)]
+def leave_condition(t, g1_queue, left_queue):
+    """
+    Condition for a student group to leave the queue.
 
-instruction.add_event([student_groups, instructors], [student_groups, instructors], end_service)
+    This function checks whether there is a student group in the queue that has
+    been waiting for more than 10 minutes. If so, it returns True; otherwise, it
+    returns False.
+
+    Parameters
+    ----------
+    t : float
+        The current time of the simulation.
+    g1_queue : list
+        The list of student groups currently in the queue.
+    left_queue : list
+        The list of student groups that have left the queue.
+    """
+
+    for g in g1_queue:
+        if t - g.time > 10:
+            return True 
+    return False 
+
+def leave_queue(t, g1_queue, left_queue):    
+    """
+    Leave the queue event function.
+
+    This function removes the student group g from g1_queue and puts it in
+    left_queue. It is called when a student group leaves the queue.
+
+    Parameters
+    ----------
+    t : float
+        The current time of the simulation.
+    g1_queue : list
+        A list of student groups waiting to be assigned to an instructor.
+    left_queue : list
+        A list of student groups that have left the queue.
+    """
+
+    for g in g1_queue:
+        if t - g.time >= 10:
+            g1_queue.remove(g)
+            left_queue.append(g)
+
+    return [g1_queue, left_queue]
+
+
+instruction.add_event([time_var, waiting.queue, left.queue],[waiting.queue, left.queue], leave_queue, guard=leave_condition)
+     
 
 # Visualisation
 
